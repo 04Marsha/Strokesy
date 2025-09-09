74a4ac9d5b48de9f103a61b7ca4453e0a97d6c75
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-09-09 10:09:45 +0530

    authentication-navbar

--- src/app/AuthenticationBar/AuthenticationBar.component.ts
+++ src/app/AuthenticationBar/AuthenticationBar.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-authenticationBar',
-    standalone: false,
-    templateUrl: './AuthenticationBar.component.html',
-    styleUrl: './AuthenticationBar.component.css'
+  selector: 'app-authenticationBar',
+  standalone: false,
+  templateUrl: './AuthenticationBar.component.html',
+  styleUrl: './AuthenticationBar.component.css',
 })
-
-export class AuthenticationBarComponent {}
+export class AuthenticationBarComponent {
+  menuOpen = false;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    console.log('menuOpen:', this.menuOpen);
+  }
+}
