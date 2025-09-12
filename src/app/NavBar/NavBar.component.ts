4f1b3a3b4d6b50f217e309f546b7e9b5000d2f29
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-09-12 18:05:17 +0530

    navbar banner

--- src/app/NavBar/NavBar.component.ts
+++ src/app/NavBar/NavBar.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-navbar',
-    standalone: false,
-    templateUrl: './NavBar.component.html',
-    styleUrl: './NavBar.component.css'
+  selector: 'app-navbar',
+  standalone: false,
+  templateUrl: './NavBar.component.html',
+  styleUrl: './NavBar.component.css',
 })
-
-export class NavBarComponent {}
+export class NavBarComponent {
+  text = 'MANDALA || ACRYLICS || WATERCOLORS || PENCIL SKETCHES || CHARCOAL || ';
+  copies = Array(2).fill(0);
+}
